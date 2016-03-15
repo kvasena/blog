279c70a25a3531ea2d75664bddb5d815eb4af440
kvasena <kvasena@gmail.com>
2016-03-16 00:08:27 +0200

    Add urls, templates and view

--- myBlog/views.py
+++ myBlog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Article
+from .models import Category
+from .models import User
+from .models import Comment
+from .models import Tag
 
-# Create your views here.
+
+def article_list(request):
+    articles = Article.objects.order_by('created')
+
+    return render(request, 'myBlog/article_list.html', {'articles': articles})
